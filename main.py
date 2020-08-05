78b3f22713f9219b38295c83eb5d77c864d10a03
Максим Марута <gideonmaxima@gmail.com>
2020-08-05 22:52:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def plus_reputation_for_member(ctx, member: discord.Member):
     for x in collection.find({"_id": member.id}):
         reps = x["rep"] = x["rep"] + 1
         collection.update_one({"_id": member.id}, {"$set": {"rep": reps}})
-        emb = discord.Embed(title = f'+rep for {member.mention}', color = 0x0000FF)
+        emb = discord.Embed(description = f'+rep for {member.mention}', color = 0x0000FF)
         await ctx.send(embed=emb)
 
 
