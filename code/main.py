e63afa1ea5347435cdf59a99e462c83df2b5f619
Максим Марута <gideonmaxima@gmail.com>
2020-08-05 15:02:19 +0300

    05.08.2020
    Add main.py and start.bat

---
+++ code/main.py
@@ -0,0 +1,9 @@
+import discord
+from discord.ext import commands
+import json
+import pymongo
+from config import settings
+
+token
+prefix = settings["prefix"]
+bot = commands.Bot(commands_prefix = '!')
