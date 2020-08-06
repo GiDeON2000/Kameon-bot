49d534e9a9aefe4001c81c4bc73d3c83a1d6ab80
Максим Марута <gideonmaxima@gmail.com>
2020-08-06 20:48:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ async def my_reps(ctx, member: discord.Member):
 
 @bot.event
 async def on_message(message):
+    await bot.process_commands(message)
     for x in collection.find({"id": message.author.id}):
         xps = x["xp"] = x["xp"] + 50
         collection.update_one({"_id": message.author.id}, {"$set": {"xp": xps}})
@@ -48,7 +49,6 @@ async def on_message(message):
             lvls = x["lvl"] = x["lvl"] + 1
             collection.update_one({"_id": message.author.id}, {"$set": {"lvl": lvls}})
             await ctx.send(f'Levelup! {x["lvl"]}')
-    await bot.process_commands(message)
 
 
 
