7b81f7a37e1107d624f8537bb11169ae40678254
Максим Марута <gideonmaxima@gmail.com>
2020-08-09 20:39:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def my_reps(ctx, member: discord.Member):
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
-	print('11111')
+	await message.channel.send('hello')
     for x in collection.find({"id": message.author.id}):
         xps = x["xp"] = x["xp"] + 50
 		print(xps)
